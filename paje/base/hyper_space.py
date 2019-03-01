e26b01a326046270a0bf042d6f807b38f17bc903
e.alcobaca <e.alcobaca@gmail.com>
2019-02-28 23:30:54 -0300

    new paje structure

---
+++ paje/base/hyper_space.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Module the doc striing.
+"""
+
+
+class HyperSpace():
+    """ TODO doc string
+    """
+    def __init__(self, hp_tree):
+        """ TODO doc string
+        """
+        self.hp_tree = hp_tree
+        self.hps = None
+
+    def tolist(self):
+        """ TODO doc string
+        """
+
+    def set_hps(self, hps):
+        """ TODO doc string
+        """
---
+++ paje/base/tree.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Module doc striing.
+"""
+
+
+class Tree():
+    """ TODO doc string
+    """
+    def __init__(self, data, children):
+        self.data = data
+        self.children = children
