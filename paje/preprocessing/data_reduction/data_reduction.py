131cc9797d3a05db81b9c7efa3c54322fb09adc1
jefferson-oliva <jeffersontalesoliva@gmail.com>
2019-03-01 10:31:14 -0300

    Adicionei o método SVD no módulo data_reduction, no qual foram removidos trechos repetivos, para os quais foram criados métodos no feature_file_processor para lidar com esse problema

--- paje/preprocessing/data_reduction/data_reduction.py
+++ paje/preprocessing/data_reduction/data_reduction.py
@@ -6,25 +6,7 @@ Author: Jefferson Tales Oliva
 
 
 '''
-This method generates the component labels. It is applied in other methods from this module
-
-Paramenter:
-
-- n_components (integer): number of components
-
-
-return: labels of components
-'''
-def genete_component_labels(n_components):
-    col = []
-    for i in range(0, n_components):
-        col.append('principal_component_' + str(i + 1))
-
-    return col
-
-
-'''
-This method is a PCA (principal component analysis) implementation. Its params is presented bellow:
+This method is a PCA (principal component analysis) implementation. Its parameters is presented bellow:
 
 - table (DataFrame): it is a feature dataset, where the lines are instances and columns are attribute-values.
 
@@ -37,40 +19,29 @@ This method is a PCA (principal component analysis) implementation. Its params i
 return: redimensioned table of features
 '''
 def apply_PCA(table, features, class_label, n_components):
-    from numpy import isnan, isinf
     from sklearn.decomposition import PCA
     from sklearn.preprocessing import StandardScaler
     from pandas import DataFrame, concat
     from paje import feature_file_processor
 
-    #x = table.loc[:, features].values
-    #y = table.loc[:, [class_label]].values
-
     # get all features and class instances
     x, y = feature_file_processor.split_features_target(table, features, class_label)
-
-    # replace nan and infinite values
-    x[isnan(x)] = 0
-    x[isinf(abs(x))] = 0
 
     # standardize features
     x = StandardScaler().fit_transform(x)
 
-    # generate pca columns
-    col = genete_component_labels(n_components)
-
     # apply PCA
     pca = PCA(n_components = n_components)
     pc = pca.fit_transform(x)
-    pDf = DataFrame(data = pc, columns = col)
 
-    return concat([pDf, table[[class_label]]], axis = 1)
+    # generate a feature table and return it
+    return feature_file_processor.generate_data_frame(pc, table[[class_label]])
 
 
 '''
 Factor analysis implementation
 
-Paramenters:
+Parameters:
 
 - table (DataFrame): it is a feature dataset, where the lines are instances and columns are attribute-values.
 
@@ -83,7 +54,7 @@ Paramenters:
 return: redimensioned table of features
 
 '''
-def apply_factor_analysis(table, features, class_label, n_comp):
+def apply_factor_analysis(table, features, class_label, n_components):
     from sklearn.decomposition import FactorAnalysis
     from pandas import DataFrame, concat
     from paje import feature_file_processor
@@ -91,11 +62,49 @@ def apply_factor_analysis(table, features, class_label, n_comp):
     # get all features and class instances
     x, y = feature_file_processor.split_features_target(table, features, class_label)
 
-    # generate factor columns
-    col = genete_component_labels(n_comp)
+    pc = FactorAnalysis(n_components = n_components, random_state=0).fit_transform(x)
 
-    factor = FactorAnalysis(n_components = n_comp, random_state=0).fit_transform(x)
+    # generate a feature table and return it
+    return feature_file_processor.generate_data_frame(pc, table[[class_label]])
 
-    pDf = DataFrame(data = factor, columns = col)
 
-    return concat([pDf, table[[class_label]]], axis = 1)
+'''
+Singular value decomposition
+
+- table (DataFrame): it is a feature dataset, where the lines are instances and columns are attribute-values.
+
+- features (list): it is a list of feature labels. The i-th feature label must correspond to a the i-th table column
+
+- class_label (string): it is the label of an attribute used as class (e.g. class, target, etc)
+
+- n_components (integer): number of features for the redimensioned table
+
+return: redimensioned table of features
+'''
+
+def apply_SVD(table, features, class_label, n_components):
+    from pandas import DataFrame, concat
+    from numpy.linalg import eig
+    from numpy import diag
+    from scipy.sparse.linalg import svds
+    from paje import feature_file_processor
+
+    # get all features and class instances
+    x, y = feature_file_processor.split_features_target(table, features, class_label)
+
+    #A = USV^T. For data reduction, the trasformation only must be US
+    # U is a m x m orthonormal matrix of 'left-singular' (eigen)vectors of  xx^T
+    #U = eig(x @ x.T)
+
+    # V is a n x n orthonormal matrix of 'right-singular' (eigen)vectors of  x^Tx
+    #lmbV, _ = eig(x.T @ x)
+
+    # S is a m x n diagonal matrix of the square root of nonzero eigenvalues of U or V
+    #S = sqrt(diag(abs(lmbV))[:n_components,:])
+
+    # apply SVD
+    u, s, _ = svds(x, n_components)
+    pc = u @ diag(s) # If we use V^T in this operation, the pc will have the original dimension
+
+    # generate a feature table and return it
+    return feature_file_processor.generate_data_frame(pc, table[[class_label]])
