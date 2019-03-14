0d131627c8e2e8acc49198093fdbc5e35c0fb59d
e.alcobaca <e.alcobaca@gmail.com>
2019-03-14 12:47:19 -0300

    requiremets added

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(name="paje",
+      version="0.0.1",
       packages=find_packages(),
+      )
+
+setup(name='paje',
+      version='0.1',
+      description='Paje automated machine leaning tool.',
+      url='https://github.com/ealcobaca/automl-paje',
+      author=["Edesio Alcobaça"],
+      author_email='e.alcobaca@gmail.com',
+      license='GPL3',
+      packages=['paje'],
+      install_requires=[
+          "scipy",
+          "numpy",
+          "scikit-learn",
+          "imbalanced-learn",
+          "liac-arff",
+          "numpy",
+          "pandas"
+      ],
       setup_requires=["pytest-runner"],
       tests_require=["pytest"],
-      )
+      zip_safe=False)
