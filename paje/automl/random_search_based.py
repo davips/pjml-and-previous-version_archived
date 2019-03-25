854214a45d29cb18f5831c34495f08e9d578743b
davips <dpsabc@gmail.com>
2019-03-25 11:26:41 -0300

    DT implemented max_features changed to real valued due to bug in sklearn max_leaf_nodes follows PA

--- paje/automl/random_search_based.py
+++ paje/automl/random_search_based.py
@@ -10,6 +10,7 @@ from paje.preprocessing.balancer.under.ran_under_sampler\
 import numpy as np
 from paje.pipeline.pipeline import Pipeline
 from paje.modelling.classifier.random_forest import RandomForest
+from paje.modelling.classifier.DT import DT
 from paje.modelling.classifier.KNN import KNN
 from paje.modelling.classifier.NB import NB
 from paje.evaluator.evaluator import Evaluator
@@ -29,7 +30,7 @@ class RadomSearchAutoML(AutoML):
         self.repetitions = repetitions
         self.prep_comp = [FilterCFS, FilterChiSquare,
                           RanOverSampler, RanUnderSampler]
-        self.mode_comp = [RandomForest, KNN, NB]
+        self.mode_comp = [RandomForest, KNN, NB, DT]
         self.comps = self.prep_comp + self.mode_comp
         self.random_state = random_state
 
--- paje/modelling/classifier/classifier.py
+++ paje/modelling/classifier/classifier.py
@@ -5,7 +5,7 @@ from paje.base.component import Component
 
 class Classifier(Component, ABC):
     def apply(self, data):
-        self.model = self.model.fit(*data.xy())
+        self.model = self.model.fit(*data.xy()) # self.model will be set in the child class
 
     def use(self, data):
         return self.model.predict(data.data_x)
