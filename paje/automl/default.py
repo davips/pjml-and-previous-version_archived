2c40052aefba4bd440c8b6703af095a232c1108c
davips <dpsabc@gmail.com>
2019-04-23 12:39:01 -0300

    Default AutoML

--- paje/automl/default.py
+++ paje/automl/default.py
@@ -2,6 +2,9 @@ from paje.automl.automl import AutoML
 
 
 class DefaultAutoML(AutoML):
+    """
+    Always select default hyperparameters.
+    """
     def next_hyperpar_dicts(self):
         dicts = [{} for _ in self.forest]
         return dicts
