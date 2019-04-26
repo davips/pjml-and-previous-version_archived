bc64b6dcc86535337dd69bece2748854a02bd71e
davips <dpsabc@gmail.com>
2019-04-26 15:28:35 -0300

    storage draft

--- paje/result/storage.py
+++ paje/result/storage.py
@@ -1,2 +1,7 @@
 class Cache:
+    """
+    This class stores and recovers results from some place.
+    The children classes are expected to provide storage in:
+     SQLite, remote/local MongoDB or MySQL server.
+    """
     pass
