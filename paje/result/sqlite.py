323ec191ca688215317046c09701e4caf35aa2e2
davips <dpsabc@gmail.com>
2019-05-16 14:43:27 -0300

    passing hash to get "data"sets

--- paje/result/sqlite.py
+++ paje/result/sqlite.py
@@ -9,13 +9,13 @@ class SQLite(Cache):
     def __init__(self, database='/tmp/paje.db'):
         self.database = database
 
-    def get_set(self, data):
+    def get_set(self, data_hash):
         """
         Extract data from database.
         :param data:
         :return:
         """
-        self.query("select data from dset where hash=?", [data.__hash__()])
+        self.query("select data from dset where hash=?", [data_hash])
         rows = self.cursor.fetchall()
         if rows is None or len(rows) == 0:
             return None
@@ -29,13 +29,13 @@ class SQLite(Cache):
                 # raise Exception('Excess of rows!')
             return SQLite.unpack(rows[0][0])
 
-    def setexists(self, data):
+    def setexists(self, data_hash):
         """
         Check if data already exists in database.
         :param data:
         :return:
         """
-        self.query("select count(1) from dset where hash=?", [data.__hash__()])
+        self.query("select count(1) from dset where hash=?", [data_hash])
         rows = self.cursor.fetchall()
         return rows is not None and len(rows) > 0
 
@@ -76,27 +76,18 @@ class SQLite(Cache):
                     print(r)
                 print('get_model: saindo do sqlite...')
                 exit(0)
-                # raise Exception('Excess of rows!')  # TODO: use general error handling
+                # TODO: use general error handling
+                # raise Exception('Excess of rows!')
             # print('rows[0][2]: ', rows[0][2])
             # print('SQLite.unpack(rows[0][2]): ', SQLite.unpack(rows[0][2]))
             return SQLite.unpack(rows[0][2])
 
-    def query(self, sql, args, debug=False):
-        if debug:
-            print(sql, args)
-        try:
-            self.cursor.execute(sql, args)
-        except Exception as e:
-            print(e)
-            print()
-            print(sql, args)
-            exit(0)
+    def get_or_else(self, component, train, f):
+        # TODO: Repeated calls to this function with the same parameters can be
+        #  memoized.
 
-    def get_or_else(self, component, train, f, test=None):
-        # TODO: Repeated calls to this function with the same parameters can be memoized.
-        # TODO: use test set for something
-
-        # TODO: check if it is possible to put this creation part inside __init__ without locking database
+        # TODO: check if it is possible to put this creation part inside
+        #  __init__ without locking database
         # TODO insert time spent
         self.connection = sqlite3.connect(self.database)
         self.cursor = self.connection.cursor()
@@ -124,7 +115,7 @@ class SQLite(Cache):
             # Processing and inserting a new combination.
             train_hash = train.__hash__()  # These two lines must be done, because train can be mutable during f().
             train_dump = SQLite.pack(train)
-            if not self.setexists(train):
+            if not self.setexists(train_hash):
                 self.query("insert into dset values (?, ?)",
                            [train_hash, train_dump])
 
@@ -146,7 +137,79 @@ class SQLite(Cache):
                         train_hash, dump], debug=False)
 
             # Store result of training data.
-            if not self.setexists(trainout):
+            if not self.setexists(trainout.__hash__()):
+                self.query("insert into dset values (?, ?)",
+                           [trainout.__hash__(), SQLite.pack(trainout)])
+            # if test is not None:
+            #     test_dump = SQLite.pack(test)
+            #     self.cursor.execute("insert into set values (?, ?)",
+            #                         [test.__hash__(), test_dump])
+            self.query("insert into out values (?, ?, ?, ?)",
+                       [type(component.__class__()).__name__,
+                        component.__hash__(), train_hash, trainout.__hash__()])
+
+            self.connection.commit()
+            res = trainout
+
+        self.connection.close()
+        return res
+
+    def get_results_or_else(self, component, train, test, f):
+        # TODO: Repeated calls to this function with the same parameters can
+        #  be memoized, to avoid network delays, for instance.
+        # TODO insert time spent
+        # TODO: check if it is possible to put this creation part inside
+        #  __init__ without locking database
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            "create table if not exists args (hash BLOB, dic BLOB)")
+        self.cursor.execute(
+            "create table if not exists dset (hash BLOB, data BLOB)")
+        self.cursor.execute(
+            "create table if not exists model (name STRING, args BLOB, train BLOB, dump BLOB)")
+        # self.cursor.execute("create table if not exists result (pipeline BLOB, train BLOB, test BLOB, prediction JSON)")
+        # TODO: check if the size of the database is too big with dumps
+        # TODO: replicability/cache of results: save testset and predictions
+        self.cursor.execute(
+            "create table if not exists out (name STRING, args BLOB, setin BLOB, setout BLOB)")
+
+        model = self.get_model(component, train)
+        if model is not None:
+            # Restoring from database.
+            # print('# Restoring from database.')
+            component.model = model
+            res = component.use(train)
+        else:
+            print('memoizing...')
+
+            # Processing and inserting a new combination.
+            # These two lines must be done, because train can be mutable during f().
+            train_hash = train.__hash__()
+            train_dump = SQLite.pack(train)
+            if not self.setexists(train_hash):
+                self.query("insert into dset values (?, ?)",
+                           [train_hash, train_dump])
+
+            # apply()
+            try:
+                trainout = f(train)
+            except Exception as e:
+                raise ExceptionInApply(e)
+
+            if not self.argsexist(component):
+                self.query("insert into args values (?, ?)",
+                           [component.__hash__(), component.serialized()])
+
+            # Store model.
+            dump = SQLite.pack(component.model)
+            self.query("insert into model values (?, ?, ?, ?)",
+                       [type(component.__class__()).__name__,
+                        component.__hash__(),
+                        train_hash, dump], debug=False)
+
+            # Store result of training data.
+            if not self.setexists(trainout.__hash__()):
                 self.query("insert into dset values (?, ?)",
                            [trainout.__hash__(), SQLite.pack(trainout)])
             # if test is not None:
@@ -165,3 +228,14 @@ class SQLite(Cache):
 
     # def __del__(self):
     #     pass
+
+    def query(self, sql, args, debug=False):
+        if debug:
+            print(sql, args)
+        try:
+            self.cursor.execute(sql, args)
+        except Exception as e:
+            print(e)
+            print()
+            print(sql, args)
+            exit(0)
--- paje/result/storage.py
+++ paje/result/storage.py
@@ -4,25 +4,17 @@ import pickle
 from abc import ABC, abstractmethod
 
 
+def uuid(description):
+    # TODO: compact with zlib uglyly to reduce the size by half
+    return hashlib.md5(description).hexdigest()
+
+
 class Cache(ABC):
     """
     This class stores and recovers results from some place.
     The children classes are expected to provide storage in:
      SQLite, remote/local MongoDB or MySQL server.
     """
-
-    @abstractmethod
-    def get_or_else(self, component, train, f, test=None):
-        """
-        Retrieve a component instance (usually a pipeline) from database if it exists.
-        Otherwise, create it calling f and store it in database.
-        :param component:
-        :param train:
-        :param f: function to execute if component has never been applied before.
-        :param test:
-        :return:
-        """
-        pass
 
     @classmethod
     def pack(cls, data):
@@ -38,7 +30,60 @@ class Cache(ABC):
         # buf.write(dump)
         # return joblib.load(buf)
 
+    @abstractmethod
+    def get_set(self, data_hash):
+        """
+        Extract data from database.
+        :param data_hash:
+        :param data:
+        :return:
+        """
 
-def uuid(description):
-    # TODO: compact with zlib uglyly
-    return hashlib.md5(description).hexdigest()
+    @abstractmethod
+    def setexists(self, data_hash):
+        """
+        Check if data already exists in database.
+        :param data:
+        :return:
+        """
+
+    @abstractmethod
+    def argsexist(self, component):
+        """
+        Check if component args already exist in database.
+        :param component:
+        :return:
+        """
+
+    @abstractmethod
+    def get_model(self, component, train):
+        """
+        Extract model from database.
+        :param component:
+        :param train:
+        :return:
+        """
+
+    @abstractmethod
+    def get_or_else(self, component, train, f):
+        """
+        Retrieve a component instance (usually a pipeline) from database if it exists.
+        Otherwise, create it calling f and store it in database.
+        :param component:
+        :param train:
+        :param f: function to execute if component has never been applied before.
+        :param test:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def get_results_or_else(self, component, train, test, f):
+        """
+        faster way of memoization
+        :param component:
+        :param train:
+        :param test:
+        :param f:
+        :return:
+        """
