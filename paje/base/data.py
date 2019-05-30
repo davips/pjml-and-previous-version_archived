54d6686b9394e49ee0e7b598ff556352015412db
davips <dpsabc@gmail.com>
2019-05-30 16:23:17 -0300

    Data now is also lazy to avoid unecessary dump packing.

--- paje/base/data.py
+++ paje/base/data.py
@@ -9,25 +9,36 @@ from paje.result.storage import uuid, pack
 
 # TODO: convert in dataclass
 class Data:
-    """
-            self.z = z  # Predictions
-        self.p = p  # Predicted probabilities
-        self.U = U  # x of unlabeled set
-        self.v = v  # y of unlabeled set
-        self.w = w  # Predictions for unlabeled set
-        self.q = q  # Predicted probabilities for unlabeled set
-    """
 
     def __init__(self, name, X=None, Y=None, Z=None, P=None,
                  U=None, V=None, W=None, Q=None,
                  columns=None):
+        """
+            Immutable lazy data for all machine learning scenarios
+             we could imagine.
+             Y, Z, V and W can be accessed as vectors y, z, v and w
+             if there is only one output.
+             Otherwise, they are multitarget matrices.
+
+        :param name:
+        :param X:
+        :param Y:
+        :param Z: Predictions
+        :param P: Predicted probabilities
+        :param U: X of unlabeled set
+        :param V: Y of unlabeled set
+        :param W: Predictions for unlabeled set
+        :param Q: Predicted probabilities for unlabeled set
+        :param columns:
+        """
         # Init instance vars and dic to factory new instances in the future.
         args = {k: v for k, v in locals().items() if k != 'self'}
         self.__dict__.update(args)
         dic = args.copy()
-        # del dic['columns']
         self._set('_dic', dic)
-        if Y is not None:
+
+        # Check list
+        if X is not None and Y is not None:
             try:
                 check_X_y(X, Y[0])
             except Exception as e:
@@ -37,21 +48,11 @@ class Data:
 
         alldata = X, Y, Z, U, V, W, P, Q
 
-
-
-        # Consider the first non None list in the args for extracting metadata.
-        def get_first_non_none(l):
-            filtered = list(filter(None.__ne__, l))
-            return [[]] if filtered == [] else filtered[0]
-
+        # Metadata
         n_classes = len(set(get_first_non_none([Y, V, Z, W])[0]))
         n_instances = len(get_first_non_none(alldata))
         n_attributes = len(get_first_non_none([X, U])[0])
         fields = {k: v for k, v in self._dic.items() if v is not None}
-        id = uuid(pack(alldata))
-
-        def dematrixfy(m):
-            return m and m[0]
 
         self.__dict__.update({
             'n_classes': n_classes,
@@ -59,12 +60,8 @@ class Data:
             'n_attributes': n_attributes,
             'Xy': (X, dematrixfy(Y)),
             'Uv': (U, dematrixfy(V)),
-            # 'predictions': {k: v for k, v in dic.items()
-            #                 if k in ['z', 'w', 'p', 'q']},
             'all': alldata,
-            'uuid': id,
             'fields': fields,
-            'name': (name or id) +'_'+','.join(non_nones)
         })
 
         # Add vectorized shortcuts for matrices.
@@ -73,12 +70,16 @@ class Data:
         for vec in vectors:
             self.__dict__[vec] = dematrixfy(self.__dict__[vec.upper()])
 
+        # Add lazy cache for dump and uuid
+        self._set('_dump', None)
+        self._set('_uuid', None)
+        self._set('_fullname', None)
+
         # TODO
         # check if exits dtype indefined == object
         # check dimensions of all matrices and vectors
 
-        # TODO: WTF is this for?
-        #  Could we check this through Noneness of z,u,v,w?
+        # TODO:   Could we check this through Noneness of z,u,v,w?
         # self._set('is_classification', False)
         # self._set('is_regression', False)
         # self._set('is_clusterization', False)
@@ -98,25 +99,16 @@ class Data:
     @staticmethod
     def read_arff(file, target):
         data = arff.load(open(file, 'r'), encode_nominal=True)
-
         columns = data["attributes"]
         df = pd.DataFrame(data['data'],
                           columns=[attr[0] for attr in data['attributes']])
-
-        Y = np.array([df.pop(target).values])
-        X = df.values.astype('float')
-
-        return Data(file, X=X, Y=Y, columns=columns)
+        return df_to_data(df, columns, file, target)
 
     @staticmethod
     def read_csv(file, target):
         df = pd.read_csv(file)
-        print(df.shape)
-        Y = np.array([df.pop(target).values])
         columns = df.columns
-        X = df.values.astype('float')
-
-        return Data(file, X=X, Y=Y, columns=columns)
+        return df_to_data(df, columns, file, target)
 
     @staticmethod
     def random(n_attributes, n_classes, n_instances):
@@ -125,20 +117,25 @@ class Data:
                                       n_classes=n_classes,
                                       n_informative=int(
                                           np.sqrt(2 * n_classes)) + 1)
-        return Data(None, X, [y])
+        return Data(None, X, as_column_vector(y))
 
     def updated(self, **kwargs):
         dic = self._dic.copy()
         dic.update(kwargs)
         for l in self.vectors:
             if l in dic:
-                dic[l.upper()] = np.array([dic.pop(l)])
+                L = l.upper()
+                dic[L] = dic.pop(L)
         return Data(**dic)
 
     def select(self, fields):
-        # Convert vectorized shortcuts to matrices.
+        """
+        Return a subset of the dictionary of kwargs.
+        ps.:Convert vectorized shortcuts to matrices.
+        :param fields:
+        :return:
+        """
         fields = [(x.upper() if x in self.vectors else x) for x in fields]
-
         return {k: v for k, v in self._dic.items() if k in fields}
 
     def sub(self, fields):
@@ -152,6 +149,50 @@ class Data:
     def _set(self, attr, value):
         object.__setattr__(self, attr, value)
 
+    def dump(self):
+        if self._dump is None:
+            self._dump = pack(self.alldata)
+        return self._dump
+
+    def uuid(self):
+        if self._uuid is None:
+            self._uuid = uuid(self.dump())
+        return self._uuid
+
+    def fullname(self):
+        if self._fullname is None:
+            self._fullname = f'unnamed[{self.uuid()}]' + \
+                             ','.join(self.fields.keys())
+        return self._fullname
+
 
 class MutabilityException(Exception):
     pass
+
+
+def as_vector(mat):
+    return mat.reshape(len(mat))
+
+
+def as_column_vector(vec):
+    return vec.reshape(len(vec), 1)
+
+
+def dematrixfy(m):
+    return m and as_vector(m)
+
+
+def get_first_non_none(l):
+    """
+    Consider the first non None list in the args for extracting metadata.
+    :param l:
+    :return:
+    """
+    filtered = list(filter(None.__ne__, l))
+    return [[]] if filtered == [] else filtered[0]
+
+
+def df_to_data(df, columns, file, target):
+    Y = as_column_vector(df.pop(target).values.astype('float'))
+    X = df.values.astype('float')
+    return Data(file, X=X, Y=Y, columns=columns)
