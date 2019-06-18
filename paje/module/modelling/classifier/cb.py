34079c62679744706cb09e9be1b27d7d086afbb4
e.alcobaca <e.alcobaca@gmail.com>
2019-06-18 18:36:54 -0300

    avoid data dependence

--- paje/module/modelling/classifier/cb.py
+++ paje/module/modelling/classifier/cb.py
@@ -17,37 +17,19 @@ class CB(Classifier):
         return data.updated(self, z=self.model.predict(data.X).flatten())
 
     def build_impl(self):
-        self.model = CatBoostClassifier(**self.dic, verbose=self.verbose)
-
-    # def apply(self, data):
-    #     try:
-    #         super().apply(data)
-    #     except:
-    #         if super().show_warnings:
-    #             print('Falling back to random classifier, if there are convergence problems (bad "nu" value, for instance).')
-    #         self.model = DummyClassifier(strategy='uniform').fit(*data.xy)
+        self.model = CatBoostClassifier(**self.dic,
+                                        verbose=self.verbose)
 
     @classmethod
     def tree_impl(cls, data):
-        # todo: inconsistent pipelines, error:
-        #  'All features are either constant or ignored.'
-        cls.check_data(data)
-        data_for_speed = {'iterations': ['z', [2, 1000]]}  # Entre outros
-        n_estimators = min(
-            [500, floor(sqrt(data.n_instances() * data.n_attributes()))])
-
         dic = {
-            'iterations': ['c', [n_estimators]],
-            # Only to set the default, not for search.
+            'iterations': ['c', [100, 500, 1000, 3000, 5000]],
             'learning_rate': ['r', [0.000001, 1.0]],
             'depth': ['z', [1, 15]],
-            # Docs says 32, but CatBoostError says 16( but is 15? )
             'l2_leaf_reg': ['r', [0.01, 99999]],
             'loss_function': ['c', ['MultiClass']],
             'border_count': ['z', [1, 255]],
-            # 'verbose': ['c', [False]],
-            # 'ctr_border_count': ['z', [1,255]], # for categorical values
-            # 'thread_count'
+            'thread_count': ['c', [-1]]
         }
 
         return HPTree(dic, children=[])
