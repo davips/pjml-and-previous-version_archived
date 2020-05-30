5eebcf2546e2388a44ea61bb5d23fdaf09af6869
ealcobaca <e.alcobaca@gmail.com>
2020-05-30 00:00:25 -0300

    [Merge] - Some updates after manual merge.

--- pjml/tool/abc/mixin/component.py
+++ pjml/tool/abc/mixin/component.py
@@ -227,30 +227,3 @@ class TComponent(Printable, Identifyable, ABC):
             return [Transformation(self, step)]
         else:
             raise BadComponent('Wrong current step:', step)
-
-
-class TTransformer:
-    def __init__(self, func, info):
-        self.func = func if func else lambda data: data
-        self._info = info
-
-        # Note:
-        # Callable returns True, if the object appears to be callable
-        # Yes, that appears!
-        if callable(self._info):
-            self.info = self._info
-        elif isinstance(self._info, dict):
-            self.info = lambda: self._info
-        elif self._info is None:
-            self.info = {}
-        else:
-            raise TypeError('Unexpected info type. You should use, callable, '
-                            'dict or None.')
-
-    def transform(self, data: Data):  # resolver error
-        # print('!!!!!!!!!!!!!!!', type(self).__name__, type(data))
-        if isinstance(data, tuple):
-            return tuple((dt.transformed(self.func) for dt in data))
-        # Todo: We should add exception handling here because self.func can
-        #  raise an error
-        return data.transformed(self.func)
