05d9226999f6130a078d007ac3f297d7cef9bdfb
ealcobaca <e.alcobaca@gmail.com>
2020-05-30 03:48:11 -0300

    [Enhancement] - New types created avoiding code rewriting.

--- pjml/util.py
+++ pjml/util.py
@@ -1,2 +1,15 @@
+from typing import NewType, Union, Tuple
+
+from pjdata.collection import Collection
+from pjdata.data import Data
+from pjdata.specialdata import NoData
+
+TDatasTuple = Union[Tuple[NoData, ...], Tuple[Data, ...],
+                    Tuple[Collection, ...],
+                    NoData, Data, Collection]
+
+TDatas = Union[NoData, Data, Collection]
+
+
 def flatten(lst):
     return [item for sublist in lst for item in sublist]
