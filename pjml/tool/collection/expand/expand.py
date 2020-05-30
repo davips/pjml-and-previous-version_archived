06c7b9894a8164428f235643cbec8c5d4dce6adf
ealcobaca <e.alcobaca@gmail.com>
2020-05-30 06:59:37 -0300

    [Enhancement] - Typing updated in class Expand.

--- pjml/tool/collection/expand/expand.py
+++ pjml/tool/collection/expand/expand.py
@@ -1,12 +1,13 @@
 from itertools import repeat
 
 from pjdata.collection import Collection
+from pjdata.data import Data
 from pjml.tool.abc.configless import ConfigLess
 from pjml.tool.abc.mixin.transformer import Transformer
 
 
 class Expand(ConfigLess):
-    def dual_transform(self, prior, posterior):
+    def dual_transform(self, prior: Data, posterior: Data):
         print(self.__class__.__name__, ' dual transf (((')
         if self.onenhancer:
             prior = Collection(repeat(prior), lambda: prior, finite=False,
@@ -16,10 +17,10 @@ class Expand(ConfigLess):
                                    finite=False, debug_info='expand')
         return prior, posterior
 
-    def _model_impl(self, prior):
+    def _model_impl(self, prior: Data) -> Transformer:
         return self._enhancer_impl()
 
-    def _enhancer_impl(self):
+    def _enhancer_impl(self) -> Transformer:
         def transform(data):
             iterator = repeat(data)
             return Collection(iterator, lambda: data, finite=False,
