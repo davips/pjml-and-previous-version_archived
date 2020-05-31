ad6ea326111910611c52e1c78252ac52f0711ccc
ealcobaca <e.alcobaca@gmail.com>
2020-05-31 19:57:31 -0300

    [Enhancement] - Substitute for the @property decorator due mypy conflicts.

--- pjml/util.py
+++ pjml/util.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Any
 
 from pjdata.collection import Collection
 from pjdata.data import Data
@@ -13,3 +13,41 @@ TDatas = Union[NoData, Data, Collection]
 
 def flatten(lst):
     return [item for sublist in lst for item in sublist]
+
+
+class Property(object):
+    """Substitute for the @property decorator due mypy conflicts"""
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+    def getter(self, fget):
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+    def setter(self, fset):
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+    def deleter(self, fdel):
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
