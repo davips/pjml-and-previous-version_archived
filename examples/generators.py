9810d0bcbc887c82168cbfa3fa8ce28f0a0fb605
davips <dpsabc@gmail.com>
2020-06-10 02:08:17 -0300

    New examples of cumulative generators utilities: resultful and force_result

--- examples/generators.py
+++ examples/generators.py
@@ -1,8 +1,14 @@
-from functools import lru_cache
+# Reduce saiu p/ bater c/ o Expand e pq pode não ser aplicado no prior.
+# def summ(prior_, posterior_):
+#     for d in posterior_:
+#         d += 1
+#     return prior_, posterior_
 
-from itertools import repeat
 
-from pjdata.collection import Collection
+#
+# from pjdata.collection import Collection
+#
+import itertools
 
 
 def pca(d):
@@ -21,44 +27,190 @@ def knn(d):
     return d * 3333
 
 
+#
+#
+#
+# def expand(data):
+#     generator = repeat(data)
+#     return Collection(generator, lambda: data)
+#
+#
+# def partition(data):
+#     generator = iter(range(4))
+#     return Collection(generator, lambda: data)
+#
+#
+# def map_(collection, f):
+#     generator = map(f, collection)
+#     return Collection(generator, lambda: collection.data)
+#
+#
+# def multi(collection, fs):
+#     generator = map(lambda f, x: f(x), fs, collection)
+#     return Collection(generator, lambda: collection.data)
+#
+#
+# def summ(collection):
+#     def generator():
+#         res = 1
+#         for data in collection:
+#             res += data
+#             yield data, res
+#
+#     return Collection(generator(), lambda res: res)
+#
+#
+# def reduce(collection):
+#     # Exhaust iterator.
+#     for _ in collection:
+#         pass
+#
+#     return collection.data
+#
+#
+# d = 2
+#
+# d2 = reduce(
+#     summ(
+#         multi(
+#             partition(d),
+#             [svm, rf, knn, pca]
+#         )
+#     )
+# )
+# print(d2)
+# print()
+#
+# print('-------------------------------------')
+# d2 = reduce(
+#     summ(
+#         multi(
+#             map_(
+#                 partition(d),
+#                 pca
+#             ),
+#             [svm, rf, knn, pca]
+#         )
+#     )
+# )
+# print(d2)
+#
+# exit()
+# class StopIteration(StopIteration):
+#     def __init__(self, a):
+#         self.args = (a,)
 
 
-def expand(data):
-    generator = repeat(data)
-    return Collection(generator, lambda: data)
+def resultful(c):
+    class Generator:
+        def __init__(self, gen):
+            self.gen = gen
+
+        # def __next__(self):
+        #     print('123 ------------------------')
+        #     try:
+        #         return next(self.gen)
+        #     except StopIteration as e:
+        #         self.result = e.args[0]
+        #         return self.result
+
+        def __iter__(self):
+            self.result = yield from self.gen
+
+    return Generator(c)
 
 
-def partition(data):
-    generator = iter(range(4))
-    return Collection(generator, lambda: data)
+class StopIterationAndReturn(Exception):
+    pass
 
 
-def map_(collection, f):
-    generator = map(f, collection)
-    return Collection(generator, lambda: collection.data)
+def force_result(gen):
+    try:
+        gen.throw(Exception)
+    except StopIteration as e:
+        return e.value
 
 
-def multi(collection, fs):
-    generator = map(lambda f, x: f(x), fs, collection)
-    return Collection(generator, lambda: collection.data)
+def repeat(d0):
+    try:
+        yield from itertools.repeat(d0)
+    finally:  # Infinite loop will be interrupted by throw().
+        return d0
 
 
-def summ(collection):
-    def generator():
-        res = 1
-        for data in collection:
-            res += data
-            yield data, res
-
-    return Collection(generator(), lambda res: res)
+def partition(d):
+    try:
+        for cc in range(1, 5):
+            yield cc
+    finally:
+        return d
 
 
-def reduce(collection):
-    # Exhaust iterator.
-    for _ in collection:
+def multi(c, fs):
+    for f, d in zip(fs, c):
+        yield f(d)
+    print('>>>>>', force_result(c))
+    return
+
+
+gen = multi(repeat(7), [pca, svm, knn])
+for d in gen:
+    print(d)
+
+# exit()
+#
+# gen = repeat(7)
+# for d in gen:
+#     print(d)
+#     gen.close()
+#     print(gen.value)
+
+print('______________________________')
+gen = resultful(partition(7))
+for d in gen:
+    print(d)
+print(gen.result)
+exit()
+
+
+def map_(c, f):
+    for d in c:
+        yield f(d)
+    return c.value
+
+
+def multi(c, fs):
+    i = 0
+    watched_gen = resultful(c)
+    for d in watched_gen:  # tratar StopException com hint sobre pipeline?
+        print('mu nex')
+        r = fs[i](d)
+        i += 1
+        if i == len(fs):
+            break
+        print('     multi gerou', r)
+        yield r
+    try:
+        next(c)
+    except StopIteration as e:
+        return e.value
+    else:  # caso infinito: então esse multi é o primeiro finito da cadeia
+        return
+
+
+def summ(c):
+    res = 1
+    for d in c:
+        res *= d
+        yield d
+    return c.value.updated(res)
+
+
+def reduce(c):
+    for d in c:
         pass
-
-    return collection.data
+        ...
+    return c.value
 
 
 d = 2
@@ -74,14 +226,15 @@ d2 = reduce(
 print(d2)
 print()
 
+# exit()
 print('-------------------------------------')
 d2 = reduce(
     summ(
         multi(
-            map_(
-                partition(d),
-                pca
-            ),
+            # map_(
+            repeat(d),
+            # pca
+            # ),
             [svm, rf, knn, pca]
         )
     )
@@ -89,60 +242,3 @@ d2 = reduce(
 print(d2)
 
 exit()
-
-
-# def partition(d):
-#     for cc in range(7):
-#         yield cc
-#     yield None  # None marca que o próximo é o pendurado.
-#     yield d
-#
-#
-# def map(c, f):
-#     for d in c:
-#         r = f(d)
-#         print('map gerou', r)
-#         if d is None:  # é bom checar se loop é infinito e dar hint.
-#             break
-#         yield r
-#     yield None
-#     yield next(c)
-#
-#     # multi vai ignorar datas excedentes!
-#
-#
-# def multi(c, fs):
-#     for f in fs:  # tratar StopException com hint sobre pipeline?
-#         d = next(c)
-#         if d is None:
-#             raise Exception('Less Data objects than expected!')
-#         r = f(d)
-#         print('     multi gerou', r)
-#         yield r
-#     yield None
-#     yield next(c)
-#
-#
-# def summ(c):
-#     res = 0
-#     for d in c:
-#         if d is None:
-#             break
-#         res *= d
-#         yield d
-#     yield None
-#     yield res
-#
-#
-# # Reduce saiu p/ bater c/ o Expand e pq pode não ser aplicado no prior.
-# # def summ(prior_, posterior_):
-# #     for d in posterior_:
-# #         d += 1
-# #     return prior_, posterior_
-#
-#
-# def reduce(c):
-#     for d in c:
-#         if d is None:
-#             break
-#     return next(c)
