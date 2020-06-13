7c1ffed81f72559905baf09c1234c5712a1b22e8
davips <dpsabc@gmail.com>
2020-06-13 06:50:29 -0300

    Label keyworded 'component=' when using field due to new arg 'block'
    ps. Block is for future networking use of lazy fields (if any).

--- pjml/config/operator/reduction/gp.py
+++ pjml/config/operator/reduction/gp.py
@@ -16,7 +16,7 @@ def full(cs, data=NoData, n=1, field='S'):
     for pipe in cs:
         p = pipe
         result_data = p.apply()
-        res = p.use(result_data).field(field, 'full search').item(0)
+        res = p.use(result_data).field(field, component='full search').item(0)
         results.append((p, -res))
 
     pipes = [x[0] for x in sorted(results, key=itemgetter(1))[:n]]
