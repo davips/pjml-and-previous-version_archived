e85b96b4e1841d75e040a69acce0747ad0967b56
ealcobaca <e.alcobaca@gmail.com>
2020-06-19 14:41:58 -0300

    Add run and a lazy version of it to crete the pipeline.

--- pjml/config/operator/reduction/full.py
+++ pjml/config/operator/reduction/full.py
@@ -1,8 +1,6 @@
 from operator import itemgetter
 
-from numpy import Inf
-
-from pjdata.specialdata import NoData
+from pjdata.content.specialdata import NoData
 from pjml.config.description.cs.configlist import ConfigList
 
 
@@ -16,13 +14,54 @@ def full(cs, data=NoData, n=1, field='S'):
 
     results = []
     for pipe in cs:
-        # try: não recomendado capturar exceções aqui, aumente ExceptionHandler!
+        pipe.dual_transform(data, data)
         model = pipe.apply()
         res = model.use(model.data).field(field, context='full search').item(0)
-        # except Exception as e:
-        #     res = -Inf
 
         results.append((pipe, -res))
 
     pipes = [x[0] for x in sorted(results, key=itemgetter(1))[:n]]
     return ConfigList(components=pipes)
+
+
+def sort(cs, data=NoData, n=1, field='S'):
+    """Exhaustive search to maximize value at 'field'.
+
+    Return 'n' best pipelines."""
+    # TODO: seed?
+    if not isinstance(cs, ConfigList):
+        raise Exception('Exhaustive search is only possible on FiniteCS!')
+
+    results = []
+    for pipe in cs:
+        pipe.dual_transform(data, data)
+        model = pipe.apply()
+        res = model.use(model.data).field(field, context='full search').item(0)
+
+        results.append((pipe, -res))
+
+    pipes = [x[0] for x in sorted(results, key=itemgetter(1))[:n]]
+    return ConfigList(components=pipes)
+
+
+def lrun(cs, train=NoData, test=NoData):
+    # TODO: seed?
+    if not isinstance(cs, ConfigList):
+        raise Exception('Exhaustive search is only possible on FiniteCS!')
+
+    for pipe in cs:
+        train_result, test_result = pipe.dual_transform(train, test)
+        yield pipe, train_result, test_result
+
+
+def run(cs, train=NoData, test=NoData):
+    # TODO: seed?
+    if not isinstance(cs, ConfigList):
+        raise Exception('Exhaustive search is only possible on FiniteCS!')
+
+    results = []
+    for pipe in cs:
+        train_result, test_result = pipe.dual_transform(train, test)
+        results.append((pipe, train_result, test_result))
+
+    return results
