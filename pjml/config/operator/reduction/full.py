0a6ab3f90b923654e382f51010e3b8a9c0ec2adb
davips <dpsabc@gmail.com>
2020-06-21 03:09:43 -0300

    Minor doc

--- pjml/config/operator/reduction/full.py
+++ pjml/config/operator/reduction/full.py
@@ -56,16 +56,32 @@ def run(cs, train=NoData, test=NoData):
 
 
 # TODO: p/ consistencia de configs, elas só vão aceitar matrizes; vou ainda atualizar os componentes
-def best(cs, n=1, train=NoData, test=NoData, better='higher'):
-    """Edesio: não sei se entendi seus objetivos, mas baseado na sua implementação condensei aqui de forma simples.
-    ps. We assume that even if training accuracy is desired,
-     it will be already transferred to the test set as 's' (or 'r' as a last resource).
-    Return 'n' best pipelines."""
-    if not isinstance(cs, ConfigList):
+#  Edesio: não sei se entendi seus objetivos, mas baseado na sua implementação condensei aqui de forma simples.
+def best(clist, n=1, train=NoData, test=NoData, better='higher'):
+    """"Sample" the 'n' best evaluation-pipelines.
+    # <-- TODO: essa terminologia faz sentido? ou devemos encontrar outro nome pro núcleo que exclui métrica (PCA->MLP)?
+    ps.1 We call the component evaluation-pipeline when it is expected to produce 's' or 'r' fields.  
+    ps.2 We assume that even if training accuracy is desired, it will be already transferred to the test set as 's'/'r'.
+    
+    Parameters
+    ----------
+    clist
+        Config list - it is a finite config space (CS)
+    n
+        number of pipelines to grab
+    train
+    test
+    better
+
+    Returns
+    -------
+        'n' best pipelines
+    """
+    if not isinstance(clist, ConfigList):
         raise Exception("Exhaustive search is only possible on FiniteCS!")
 
     def dual(component):
         return component.dual_transform(train, test)[1], component
 
     select = nlargest if better == 'higher' else nsmallest
-    return ConfigList(components=map(_[1], select(n, map(dual, cs))))
+    return ConfigList(components=map(_[1], select(n, map(dual, clist))))
