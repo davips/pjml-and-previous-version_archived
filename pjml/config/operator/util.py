bcbc7a19459db72881d31142f2da36e6d064ccac
ealcobaca <e.alcobaca@gmail.com>
2020-06-23 00:21:08 -0300

    Add functions to handle experiments.

--- pjml/config/operator/util.py
+++ pjml/config/operator/util.py
@@ -1,53 +1,80 @@
-from operator import itemgetter
+from collections import Iterable
+from heapq import nsmallest, nlargest
 
+from pjdata.aux.util import _
 from pjdata.content.specialdata import NoData
 from pjml.config.description.cs.configlist import ConfigList
 
 
-def run(cs, train=NoData, test=NoData):
-    if not isinstance(cs, ConfigList):
+def compare(x, y):
+    if isinstance(x, Iterable) and isinstance(y, Iterable):
+        return all(compare(i, j) for i, j in zip(x, y))
+    return x.isequal(y)
+
+
+def slice(clist, start=0.75, end=1.0):
+    return sort(clist, train=train, test=test,)
+
+
+def run(clist, train=NoData, test=NoData):
+    if not isinstance(clist, ConfigList):
         raise Exception("Exhaustive search is only possible on FiniteCS!")
 
     results = []
-    for pipe in cs:
+    for pipe in clist:
         train_result, test_result = pipe.dual_transform(train, test)
         results.append((pipe, train_result, test_result))
 
     return results
 
 
-def get(iterator, function=lambda x: (x[1].s, x[2])):
-    for it in iterator:  # <-- essa ideia é tão boa que a função map() virou um padrão p/ isso [davi]
-        yield function(it)
-
-
-def lrun(cs, train=NoData, test=NoData):
-    if not isinstance(cs, ConfigList):
+def lrun(clist, train=NoData, test=NoData):
+    if not isinstance(clist, ConfigList):
         raise Exception("Exhaustive search is only possible on FiniteCS!")
 
-    for pipe in cs:
+    for pipe in clist:
         train_result, test_result = pipe.dual_transform(train, test)
-        yield train_result, test_result, pipe
+        yield pipe, train_result, test_result
 
 
-def sort(iterator, key=itemgetter(0), reverse=False):
+def sort(clist, train=NoData, test=NoData, key=lambda x: x[2].S, reverse=False):
     """Exhaustive search to maximize value at 'field'.
 
     Return 'n' best pipelines."""
-    return sorted(iterator, key=key, reverse=reverse)
+
+    def iterator():
+        for r in lrun(clist, train, test):
+            yield key(r)
+
+    return sorted(iterator(), key=key, reverse=reverse)
 
 
-def maximize(cs, n=1, train=NoData, test=NoData):
-    return min_max_pipe(cs=cs, n=n, train=train, test=test, reverse=True)
+def best(clist, n=1, train=NoData, test=NoData, better="higher"):
+    print(type(clist))
+    if not isinstance(clist, ConfigList):
+        raise Exception("Exhaustive search is only possible on FiniteCS!")
+    higher = "higher"
+    smaller = "smaller"
+
+    select = None
+    if better == "higher":
+        select = nlargest
+    elif better == "smaller":
+        select = nsmallest
+    else:
+        raise ValueError(
+            f"Expected '{higher}' or '{smaller}' in 'better', but was given '{better}'"
+        )
+
+    def dual(component):
+        return component.dual_transform(train, test)[1], component
+
+    return ConfigList(components=map(_[1], select(n, map(dual, clist))))
 
 
-def minimize(cs, n=1, train=NoData, test=NoData):
-    return min_max_pipe(cs=cs, n=n, train=train, test=test)
+def maximize(clist, n=1, train=NoData, test=NoData):
+    return best(clist, n=n, train=train, test=test, better="smaller")
 
 
-def min_max_pipe(cs, n=1, train=NoData, test=NoData, reverse=False):
-    return ConfigList(
-        components=[
-            i[1] for i in sort(get(lrun(cs, train, test)), reverse=reverse)[0:n]
-        ]
-    )
+def minimize(clist, n=1, train=NoData, test=NoData):
+    return best(clist, n=n, train=train, test=test, better="higher")
